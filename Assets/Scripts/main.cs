5708b40a677e9ad9721e82adaa6f9922521ba411
Miguel Jiménez Gomis <jimenezgomismiguel@gmail.com>
2017-11-17 17:41:22 +0000

    Inicio
    Inicio del juego y pruebas

---
+++ Assets/Scripts/main.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class main : MonoBehaviour {
+    public GameObject Fondo;
+    Image myimagecomponent;
+    public Sprite habitacion;
+    public Text input;
+
+    private void Start()    {
+        myimagecomponent = Fondo.GetComponent<Image>();
+    }
+    void Update () {}
+
+    public void inicio () {
+        myimagecomponent.sprite = habitacion;
+        input.text = "hola";
+    }
+}
